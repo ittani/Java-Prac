94dd9aa06637218be9b481dfdd452052a0c99eb0
Ittani Maemo <ittanim@gmail.com>
2025-01-13 10:19:51 +0200

    Update Schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,19 @@ create table course
 	primary key (id)
 
 );
+
+create table employee
+(
+    id bigint not null,   -- Use identity for auto-incrementing
+    name varchar(255) not null,
+    primary key (id)
+);
+
+create table Student
+(
+id bigint not null,
+name varchar(255) not null,
+StudentId varchar(255) not null,
+location varchar(255) not null,
+primary key(id)
+);
